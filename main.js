3873f8c420cd4dcaa62ff0bb2780765817c73e24
Crystal To <crystal.kyto@gmail.com>
2014-10-30 23:57:51 -0700

    Cleanup of variables and fixed up/down animations

--- main.js
+++ main.js
@@ -2,16 +2,14 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 var score = 0;
 var highScore = 0;
 var scoreText;
-var endScore;
 var playing;
-var gameover;
-
+var gameoverText;
 var pspeed = 150;
 
 function preload() {
 	game.load.image('star', 'assets/beer.png');
 	game.load.spritesheet('dude', 'assets/pedestrian2.png', 32, 48);
-	game.load.spritesheet('baddie', 'assets/bike2.png', 40, 40);
+	game.load.spritesheet('bike', 'assets/bike2.png', 40, 40);
 	game.load.spritesheet('boarder', 'assets/boarder.png', 32, 40);
 	game.load.spritesheet('walker', 'assets/walker.png', 32, 48);
 }
@@ -65,7 +63,7 @@ function update() {
 		baddies.forEachExists(stopBaddie, this);	
 		if (keyboard.isDown(32)) {
 			baddies.forEachExists(killBaddie, this);
-			gameover.destroy(true);
+			gameoverText.destroy(true);
 			score = 0;
 			collectStar(player, star);
 			pspeed = 150;
@@ -93,29 +91,26 @@ function updateScore() {
 }
 
 function controlPlayer() {
-
-	if (cursors.up.isDown) {
-		player.body.velocity.y = -pspeed;
-		player.animations.stop();
-		player.frame = 4;
-	} else if (cursors.down.isDown) {
-		player.body.velocity.y = pspeed;
-		player.animations.stop();
-		player.frame = 4;
-	} 
-
 	if (cursors.left.isDown) {
 		player.body.velocity.x = -pspeed;
 		player.animations.play('left');
 	} else if (cursors.right.isDown) {
 		player.body.velocity.x = pspeed;
 		player.animations.play('right');
-	} else {
+	} 
+
+	if (cursors.up.isDown) {
+		player.body.velocity.y = -pspeed;
+	} else if (cursors.down.isDown) {
+		player.body.velocity.y = pspeed;
+	} 
+
+
+	if (!cursors.up.isDown && !cursors.down.isDown
+		&& !cursors.left.isDown && !cursors.right.isDown) {
 		player.animations.stop();
-		player.frame = 4;
+		player.frame = 4;	
 	}
-
-
 }
 
 function addBaddie() {
@@ -133,7 +128,7 @@ function addBaddie() {
 	var speed;
 
 	if (type == 0) {
-		var baddie = baddies.create(x, y, 'baddie');	
+		var baddie = baddies.create(x, y, 'bike');	
 		baddie.animations.add('left', [0,1], 10, true);
 		baddie.animations.add('right', [2,3], 10, true);
 		speed = 300;
@@ -160,11 +155,11 @@ function addBaddie() {
 			break;
 		case 2:
 			baddie.body.velocity.y = speed;
-			baddie.frame = 0;
+			baddie.animations.play('right');
 			break;
 		case 3:
 			baddie.body.velocity.y = -speed;
-			baddie.frame = 0;
+			baddie.animations.play('left');
 			break;
 	}
 }
@@ -206,15 +201,17 @@ function moveBaddie(baddie) {
 	} else {
 		if(baddie.y > (game.world.height - 32)) {
 			baddie.body.velocity.y = -speed;
+			baddie.animations.play('left');
 		} else if (baddie.y < 0) {
 			baddie.body.velocity.y = speed;
+			baddie.animations.play('right');
 		}
 	}	
 }
 
 function gameOver(player, baddies) {
 	playing = false;
-	gameover = game.add.text(400, game.world.height - (32*3) - 16, 
+	gameoverText = game.add.text(400, game.world.height - (32*3) - 16, 
 		'GAME OVER\nYour Score: ' + score + '\nPress spacebar to play again', 
 		{fontSize: '32px', fill: '#000', align: 'right'});
 	if (score > highScore) {
